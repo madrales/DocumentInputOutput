bdf28d124c6de707b4bb646d5ce31afc1e13cf1a
Mason Adrales <masonadrales@mwc-106087.dhcp.missouri.edu>
2019-02-14 00:47:39 -0600

    Adding text file samples

---
+++ DocumentIO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace DocumentIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Document" + " ");
+                Console.ReadLine();
+                Console.WriteLine("What is the name of the document?");
+                name = Console.ReadLine();
+                FileStream F = new FileStream(name + ".txt", FileMode.OpenorCreate, FileAccess.ReadWrite);
+            } catch (System.IO.IOException) {
+                Console.WriteLine("The file can not be opened or created.");
+                System.Environment.Exit(1);
+            }
+                finally {
+                Console.WriteLine("What information would you like to put into the document?");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ DocumentIO/DocumentIO/DocumentIO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace DocumentIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Document" + " ");
+                Console.WriteLine("What is the name of the document?");
+                String name = Console.ReadLine();
+                String fileName = (name + ".txt");
+                Console.WriteLine("What information would you like to put into the document?");
+                String input = Console.ReadLine();
+                using (StreamWriter writer = new StreamWriter(fileName, true))
+                {
+                    writer.Write(input);
+                }
+                FileStream fs = new FileStream(input, FileMode.Append);
+                System.IO.File.WriteAllText(fileName, input);
+                string count = File.ReadAllText(fileName);
+                Console.WriteLine(fileName + " was a successfully saved. The document contains " + count.Length + " characters.");
+                System.Environment.Exit(1);
+                /*FileStream F = new FileStream(name + ".txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);*/
+            }
+            catch (System.IO.IOException)
+            {
+                Console.WriteLine("The file can not be opened or created.");
+                System.Environment.Exit(1);
+            }
+
+
+        }
+    }
+}
